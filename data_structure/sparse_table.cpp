6b98127aaef7cba8a37cf6cf0e117b61027c8a5c
newfangledmindset <newfangledmindset@gmail.com>
2024-09-06 19:43:16 +0900

    sparse table

--- data_structure/sparse_table.cpp
+++ data_structure/sparse_table.cpp
@@ -42,7 +42,7 @@ void preprocess() {
     for (int i = 1; i <= K; i++)
         for (int j = 0; j + (1 << i) <= N; j++)
             // 여기선 idempotent 함수로 min을 썼다
-            st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1)) - 1]);
+            st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
 }
 
 int log_2(const ll i) {
